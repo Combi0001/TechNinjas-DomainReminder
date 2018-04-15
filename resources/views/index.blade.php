9f443fbd32392df4e85aa687d0e539b7a8f7166a
Unknown <s3661342@student.rmit.edu.au>
2018-04-15 13:58:35 +1000

    Added all of Joey's template files to the correct directories
    Setup the home page template and all of the scripts

---
+++ resources/views/index.blade.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="generator" content="DomainReminder">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link rel="shortcut icon" href="/img/icon-60x60.png" type="image/x-icon">
+        <meta name="description" content="">
+        <title>Home</title>
+        <link rel="stylesheet" href="/css/scripts/tether/tether.min.css">
+        <link rel="stylesheet" href="/css/scripts/bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/scripts/bootstrap/bootstrap-grid.min.css">
+        <link rel="stylesheet" href="/css/scripts/bootstrap/bootstrap-reboot.min.css">
+        <link rel="stylesheet" href="/css/scripts/dropdown/style.css">
+        <link rel="stylesheet" href="/css/custom/index.css">
+        <link rel="stylesheet" href="/css/custom/mbr-additional.css" type="text/css">
+
+
+
+    </head>
+    <body>
+    <section class="menu cid-qP54ZFFg5f" once="menu" id="menu1-j">
+
+
+
+        <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="menu-logo">
+                <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://domainreminder.net">
+                         <img src="/img/icon-60x60.png" alt="domainrem" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                    <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="https://domainreminder.net/">DomainReminder</a></span>
+                </div>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+                        <a class="nav-link link text-white display-4" href="https://domainreminder.net/">
+                            <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link text-white display-4" href="https://domainreminder.net"><span class="mbri-info mbr-iconfont mbr-iconfont-btn"></span>
+                            Information &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
+                    </li></ul>
+                <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-primary display-4" href="https://domainreminder.net/#"><span class="mbri-login mbr-iconfont mbr-iconfont-btn"></span>
+                        Login / Register</a></div>
+            </div>
+        </nav>
+    </section>
+
+    <section class="engine"><a href="https://domainrem.ws/c">best free site maker</a></section><section class="tabs1 cid-qP578ue5kZ" id="tabs1-m">
+
+
+
+
+        <div class="container">
+            <h2 class="mbr-white align-center pb-5 mbr-fonts-style mbr-bold display-2">LOREM IPSUM YOUR DOMAIN IS YOURS</h2>
+            <div class="media-container-row">
+                <div class="col-12 col-md-8">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item"><a class="nav-link mbr-fonts-style active show display-7" role="tab" data-toggle="tab" href="#tabs1-m_tab0" aria-selected="true">
+                                Never Forget</a></li>
+                        <li class="nav-item"><a class="nav-link mbr-fonts-style active show display-7" role="tab" data-toggle="tab" href="#tabs1-m_tab1" aria-selected="true">
+                                We Remind YOU</a></li>
+                        <li class="nav-item"><a class="nav-link mbr-fonts-style active show display-7" role="tab" data-toggle="tab" href="#tabs1-m_tab2" aria-selected="true">ALWAYS FREE</a></li>
+
+
+
+                    </ul>
+                    <div class="tab-content">
+                        <div id="tab1" class="tab-pane in active" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p class="mbr-text py-5 mbr-fonts-style display-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="tab2" class="tab-pane" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p class="mbr-text py-5 mbr-fonts-style display-7">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="tab3" class="tab-pane" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p class="mbr-text py-5 mbr-fonts-style display-7">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequats.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="features8 cid-qP586FcY0k" id="features8-o">
+
+
+
+
+
+        <div class="container">
+            <div class="media-container-row">
+
+                <div class="card  col-12 col-md-6 col-lg-4">
+                    <div class="card-img">
+                        <span class="mbri-desktop mbr-iconfont"></span>
+                    </div>
+                    <div class="card-box align-center">
+                        <h4 class="card-title mbr-fonts-style display-7">
+                            Unlimited Sites
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="mbr-section-btn text-center"><a href="https://domainreminder.net/" class="btn btn-secondary display-4">
+                                More
+                            </a></div>
+                    </div>
+                </div>
+
+                <div class="card  col-12 col-md-6 col-lg-4">
+                    <div class="card-img">
+                        <span class="mbri-touch mbr-iconfont"></span>
+                    </div>
+                    <div class="card-box align-center">
+                        <h4 class="card-title mbr-fonts-style display-7">
+                            Mobile Friendly
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="mbr-section-btn text-center"><a href="https://domainreminder.net/" class="btn btn-secondary display-4">
+                                More
+                            </a></div>
+                    </div>
+                </div>
+
+                <div class="card  col-12 col-md-6 col-lg-4">
+                    <div class="card-img">
+                        <span class="mbri-responsive mbr-iconfont"></span>
+                    </div>
+                    <div class="card-box align-center">
+                        <h4 class="card-title mbr-fonts-style display-7">
+                            Easy Navigation</h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <div class="mbr-section-btn text-center"><a href="https://domainreminder.net/" class="btn btn-secondary display-4">
+                                More
+                            </a></div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+    <section once="" class="cid-qP56U1Oxmu" id="footer6-l">
+
+
+
+
+
+        <div class="container">
+            <div class="media-container-row align-center mbr-white">
+                <div class="col-12">
+                    <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                        © Copyright 2018 DomainReminder - All Rights Reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <script src="/js/scripts/jquery/jquery.min.js"></script>
+    <script src="/js/scripts/popper/popper.min.js"></script>
+    <script src="/js/scripts/tether/tether.min.js"></script>
+    <script src="/js/scripts/bootstrap/bootstrap.min.js"></script>
+    <script src="/js/scripts/smoothscroll/smooth-scroll.js"></script>
+    <script src="/js/scripts/dropdown/script.min.js"></script>
+    <script src="/js/scripts/touchswipe/jquery.touch-swipe.min.js"></script>
+    <script src="/js/scripts/mbr-tabs/mbr-tabs.js"></script>
+    <script src="/js/custom/index.js"></script>
+
+
+    </body>
+</html>
