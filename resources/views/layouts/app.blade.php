2b66c2227275cd00a9a4dfe003b28ee4d232cf11
Alex Mills <s3688859@student.rmit.edu.au>
2018-05-06 18:49:30 +1000

    - Apparently I needed to add npm so I can work with SASS - Threw in a logo I was fucking around with, it's not great - Added the colours we're using to SASS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="/css/scripts/dropdown/style.css">
     <link rel="stylesheet" href="/css/custom/index.css">
     <link rel="stylesheet" href="/css/custom/mbr-additional.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
 
@@ -38,7 +39,7 @@
     <div class="container align-center">
         <span class="text-muted">© Copyright 2018 DomainReminder - All Rights Reserved
             <div>
-                <a href="/about">About</a>
+                <a href="/about">About</a> &middot;
                 <a href="/contact">Contact</a>
             </div>
         </span>
