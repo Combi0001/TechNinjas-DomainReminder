b3a054a9cc84e2b13e35ee2d9526cbf40d006a4a
Adam <s3661342@student.rmit.edu.au>
2018-05-25 22:51:44 +1000

    Fixed verification

--- resources/views/verification.blade.php
+++ resources/views/verification.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(Auth::user())
-        <div>
-            Verification email has been sent to {{Auth::user()->defaultEmail()->email}}
-            Please verify email
-        </div>
-    @else
-        Error
-    @endif
+    <?php
+        if (!isset($user)) {
+            $user = Auth::user();
+        }
+    ?>
+    <div>
+        Verification email has been sent to {{$user->defaultEmail()->email}}<br/>
+        Please verify email
+    </div>
 @endsection
